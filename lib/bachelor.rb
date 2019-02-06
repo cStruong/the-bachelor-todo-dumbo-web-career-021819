f22787142162b8688eea4ef4971330718682d045
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-06 17:57:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,19 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+   
+  data.each { |seasons, peopleArr| 
+    if seasons == season
+      peopleArr.each { |person| 
+        person.each { |info, info_data|
+          if info_data == 'Winner'
+            nameArr = person["name"].split(" ")
+            return nameArr[0]
+          end
+        }
+      }
+    end
+  }
 end
 
 def get_average_age_for_season(data, season)
