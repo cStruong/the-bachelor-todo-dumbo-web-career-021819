260f7d2384b6c99b0a96381fd4c982438bad15c3
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-06 18:02:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -69,7 +69,7 @@ def get_average_age_for_season(data, season)
     peopleArr.each { |person| 
       person.each { |info, info_data|
         if info == "age"
-          sum += age.to_i
+          sum += info.to_i
           count += 1
         end
       }
