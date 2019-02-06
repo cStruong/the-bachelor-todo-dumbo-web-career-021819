bb89989d84c43d4dcaa0eeae9aec0c8693d2d8c4
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-02-06 17:53:40 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  
+  data.each { |seasons, peopleArr| 
+    if seasons == season
+      peopleArr.each { |person| 
+        person.each { |info, info_data|
+          if info_data == 'Winner'
+            nameArr = person["name"].split(" ")
+            return nameArr[0]
+          end
+        }
+      }
+    end
+  }
+  
 end
 
 def get_contestant_name(data, occupation)
